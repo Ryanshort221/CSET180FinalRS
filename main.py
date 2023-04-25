b5b083315f1f820d48ff42f387dc4a54a73d5920
Ryan Short <ryanshort55@gmail.com>
2023-04-25 11:05:31 -0400

    made some minor changes to html and python

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
-from flask import Flask, render_template, request
-from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
+from flask import Flask, render_template, request, session
+from sqlalchemy import create_engine, text
 import hashlib
 from decimal import Decimal
 
 
 app = Flask(__name__)
+app.secret_key = 'apple'
+
+def encryptor(password):
+    salt = 'apple'
+    encrypted_password = hashlib.sha224(password.encode('utf-8')).hexdigest() + salt
+    return encrypted_password
+
 
 conn_str = 'mysql://root:1012@localhost/cset180final'
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
+
+
+@app.before_request
+def before_request():
+    session.clear()
 
 
 @app.route('/')
@@ -18,3 +30,8 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
