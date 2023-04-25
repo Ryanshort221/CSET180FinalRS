687d1d39f7ac699227cfb175b79e438c7275d924
Ryan Short <ryanshort55@gmail.com>
2023-04-25 19:05:09 -0400

    added flash msgs user profile started vendor/admin page

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, flash
 from sqlalchemy import create_engine, text
 import hashlib
 from decimal import Decimal
@@ -19,9 +19,9 @@ engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
 
-@app.before_request
-def before_request():
-    session.clear()
+# @app.before_request
+# def before_request():
+#     session.clear()
 
 
 @app.route('/')
@@ -56,23 +56,52 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = encryptor(request.form['password'])
-        result = conn.execute(text('select * from users where username=:username and password=:password').params(username=username, password=password))
-        if result.rowcount == 1:
+        user_type = request.form['user_type']
+        result = conn.execute(text('select * from users where username=:username and password=:password and user_type=:user_type').params(username=username, password=password, user_type=user_type))
+        if result.rowcount == 1 and user_type == 'customer':
             session['username'] = username
+            flash(f"You successfully logged in as {username}")
             return redirect('products')
-        else:
-            return render_template('index.html', message='incorrect login credentials try again')
+        elif result.rowcount == 1 and user_type == 'vendor':
+            session['username'] = username
+            flash(f"Vendor {username} successfully logged in")
+            return redirect('vendor')
+        elif result.rowcount == 1 and user_type == 'admin':
+            session['username'] = username
+            flash(f"Admin {username} successfully logged in")
+            return render_template('admin.html', username=username)
+        elif result.rowcount == 0:
+            flash("Invalid login credentials please try again", 'error')
+            return redirect('/')
 
 
 @app.route('/logout', methods=['POST', 'GET'])
 def logout():
-    session.pop['username', None]
-    return render_template('index.html')
+    session.pop('username', None)
+    flash("Successfully logged out")
+    return redirect('/')
 
 
 @app.route('/products', methods=['POST', 'GET'])
 def products():
     return render_template('products.html')
+
+
+@app.route('/vendor', methods=['POST', 'GET'])
+def vendor():
+    return render_template('vendor.html')
+
+
+@app.route('/admin', methods=['POST', 'GET'])
+def admin():
+    return render_template('admin.html')
+
+
+@app.route('/user_profile')
+def profile():
+    username = session['username']
+    result = conn.execute(text('select username, first_name, last_name, email, user_type from users where username=:username ').params(username=username))
+    return render_template('user_profile.html', result=result)
 
 
 if __name__ == '__main__':
