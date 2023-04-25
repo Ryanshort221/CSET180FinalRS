18f3f8d26aade5685d47f0781eb86fb1b7b71583
Ryan Short <ryanshort55@gmail.com>
2023-04-25 09:12:50 -0400

    second commit

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sqlalchemy import Column, Integer, String, Numeric, create_engine, text
 
 app = Flask(__name__)
 
-conn_str = 'mysql://root:1012@localhost/cset170final'
+conn_str = 'mysql://root:1012@localhost/cset180final'
 engine = create_engine(conn_str, echo=True)
 conn = engine.connect()
 
