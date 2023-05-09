2baa3ca456c2da86760040fa4a05cae879f590bb
Ryan Short <ryanshort55@gmail.com>
2023-05-08 22:26:14 -0400

    finished chat need to design a lot add js for buttons and chat windows

--- main.py
+++ main.py
@@ -615,5 +615,66 @@ def update_complaints():
         return redirect('complaints')
 
 
+@app.route('/chat', methods=['POST', 'GET'])
+def chat():
+    if request.method == 'GET':
+        if session['username'] == 'vendor':
+            user_id = session['user_id']
+            admins = conn.execute(text('select user_id, username from users where user_type="admin"'))
+            customers = conn.execute(text('select user_id, username from users where user_type="customer"'))
+            messages = conn.execute(text('select * from chat')).fetchall()
+            chats = conn.execute(text('select c.customer_id, c.admin_vendor_id, c.title, c.date, u.username from chat c join users u on customer_id=user_id where message is null and admin_vendor_id=:user_id').params(user_id=user_id)).fetchall()
+            return render_template('chat.html', admins=admins, customers=customers, chats=chats, messages=messages)
+        elif session['username'] == 'admin':
+            user_id = session['user_id']
+            vendors = conn.execute(text('select user_id, username from users where user_type="vendor"'))
+            customers = conn.execute(text('select user_id, username from users where user_type="customer"'))
+            messages = conn.execute(text('select * from chat')).fetchall()
+            chats = conn.execute(text('select c.customer_id, c.admin_vendor_id, c.title, c.date, u.username from chat c join users u on customer_id=user_id where message is null and admin_vendor_id=:user_id').params(user_id=user_id)).fetchall()
+            return render_template('chat.html', vendors=vendors, customers=customers, chats=chats, messages=messages)
+        else:
+            user_id = session['user_id']
+            admins = conn.execute(text('select user_id, username from users where user_type="admin"'))
+            vendors = conn.execute(text('select user_id, username from users where user_type="vendor"'))
+            messages = conn.execute(text('select * from chat')).fetchall()
+            chats = conn.execute(text('select c.customer_id, c.admin_vendor_id, c.title, c.date, u.username from chat c join users u on admin_vendor_id=user_id where message is null and customer_id=:user_id').params(user_id=user_id)).fetchall()
+            return render_template('chat.html', admins=admins, vendors=vendors, chats=chats, messages=messages)
+
+
+@app.route('/start_chat', methods=['POST'])
+def start_chat():
+    user_id = session['user_id']
+    conn.execute(text('insert into chat (customer_id, admin_vendor_id, title, date) values(:user_id, :admin_vendor_id, :title, curdate())').params(user_id=user_id), request.form)
+    conn.commit()
+    return redirect('chat')
+
+
+@app.route('/send_message_customer', methods=['POST'])
+def send_message():
+    if request.method == 'POST':
+        user_id = session['user_id']
+        conn.execute(text('insert into chat (customer_id, sender_id, admin_vendor_id, receiver_id, message, date) values(:user_id, :user_id, :admin_vendor_id, :admin_vendor_id, :message, curdate())').params(user_id=user_id), request.form)
+        conn.commit()
+        return redirect('chat')
+
+
+@app.route('/send_message_admin', methods=['POST'])
+def send_message_admin():
+    if request.method == 'POST':
+        user_id = session['user_id']
+        conn.execute(text('insert into chat (admin_vendor_id, sender_id, customer_id, receiver_id, message, date) values(:user_id, :user_id, :customer_id, :customer_id, :message, curdate())').params(user_id=user_id), request.form)
+        conn.commit()
+        return redirect('chat')
+
+
+@app.route('/send_message_vendor', methods=['POST'])
+def send_message_vendor():
+    if request.method == 'POST':
+        user_id = session['user_id']
+        conn.execute(text('insert into chat (admin_vendor_id, sender_id, customer_id, receiver_id, message, date) values(:user_id, :user_id, :customer_id, :customer_id, :message, curdate())').params(user_id=user_id), request.form)
+        conn.commit()
+        return redirect('chat')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
